7a6e4a7e8e606a960d7ee39dc9b0fc45599e5ab6
desracto <theminer123321@gmail.com>
2023-03-05 16:19:49 +0400

    Clean up

--- Project/Project.cpp
+++ Project/Project.cpp
@@ -5,12 +5,13 @@
 
 std::vector<std::vector<int>> load_array_vec()
 {
-	// make sure to change the path
+  // make sure to change the path
   std::ifstream file("F:\\Programs\\Onedrive\\OneDrive - University of Wollongong\\Studies\\2023 Winter - 203, 317, 214\\CSCI 203\\Assignment\\Assignment 2\\map_844x480.dat");
-	std::vector<std::vector<int>> arr(480, std::vector<int>(844)); // Row count, column count
+  std::vector<std::vector<int>> arr(480, std::vector<int>(844)); // Row count, column count
   
 	try
 	{
+		std::string word;
 		for (int r = 0; r < 480; r++)
 		{
 			for (int c = 0; c < 844; c++)
