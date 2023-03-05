42f44f6df0a5bb4f179e214d45c535b2a1ef2af0
desracto <theminer123321@gmail.com>
2023-03-05 16:31:51 +0400

    Fix error

--- Project/Project.cpp
+++ Project/Project.cpp
@@ -6,9 +6,10 @@
 std::vector<std::vector<int>> load_array_vec()
 {
 	// make sure to change the path
-  std::ifstream file("F:\\Programs\\Onedrive\\OneDrive - University of Wollongong\\Studies\\2023 Winter - 203, 317, 214\\CSCI 203\\Assignment\\Assignment 2\\map_844x480.dat");
+	std::ifstream file("F:\\Programs\\Onedrive\\OneDrive - University of Wollongong\\Studies\\2023 Winter - 203, 317, 214\\CSCI 203\\Assignment\\Assignment 2\\map_844x480.dat");
 	std::vector<std::vector<int>> arr(480, std::vector<int>(844)); // Row count, column count
-  
+	std::string word;
+
 	try
 	{
 		for (int r = 0; r < 480; r++)
