8abed335fe76e95c7e05d5766ee96c637b467025
desracto <theminer123321@gmail.com>
2023-03-05 15:18:32 +0400

    Test Pull

--- Project/Project.cpp
+++ Project/Project.cpp
@@ -6,36 +6,6 @@
 #include <istream>
 #include <array>
 
-int (*load_array_2D())[844]
-{
-	std::fstream file("F:\\Programs\\Onedrive\\OneDrive - University of Wollongong\\Studies\\2023 Winter - 203, 317, 214\\CSCI 203\\Assignment\\Assignment 2\\map_844x480.dat", std::ios::in);
-
-	int (*arr)[844] = new int[480][844];
-	std::string word;
-	try 
-	{
-		// File is column by row
-		// Array needs to be row by column
-		// This transposes the file automatically as it will 
-		// select a column first then incremet row by row
-		for (int r = 0; r < 480; r++)
-		{
-			for (int c = 0; c < 844; c++)
-			{
-				file >> word;
-				arr[r][c] = stoi(word);
-			}
-		}
-		return arr;
-	}
-	catch (std::exception& e)
-	{
-		std::cout << e.what();
-		return arr;
-	}
-
-}
-
 std::vector<std::vector<int>> load_array_vec()
 {
 	std::fstream file("F:\\Programs\\Onedrive\\OneDrive - University of Wollongong\\Studies\\2023 Winter - 203, 317, 214\\CSCI 203\\Assignment\\Assignment 2\\map_844x480.dat", std::ios::in);
@@ -58,32 +28,6 @@ std::vector<std::vector<int>> load_array_vec()
 	catch (std::exception& e)
 	{
 		std::cout << "EXCEPTION: " << e.what();
-		return arr;
-	}
-}
-
-std::vector<int> load_array_1d()
-{
-	std::fstream file("F:\\Programs\\Onedrive\\OneDrive - University of Wollongong\\Studies\\2023 Winter - 203, 317, 214\\CSCI 203\\Assignment\\Assignment 2\\map_844x480.dat", std::ios::in);
-	std::vector<int> arr(480*844);
-	std::string word;
-	const int WIDTH = 844;
-
-	try
-	{
-		for (int r = 0; r < 480; r++)
-		{
-			for (int c = 0; c < 844; c++)
-			{
-				file >> word;
-				arr[WIDTH * r + c] = stoi(word);
-			}
-		}
-		return arr;
-	}
-	catch (std::exception& e)
-	{
-		std::cout << e.what();
 		return arr;
 	}
 }
